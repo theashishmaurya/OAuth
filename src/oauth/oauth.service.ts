5af74a7011d87e1cf6350bfad39ee0c2ff38cb73
AshishIvoyant <ashish.1999vns@gmail.com>
2024-02-04 16:55:10 +0530

    Add in-memory authorization code strategy to OAuthService

--- src/oauth/oauth.service.ts
+++ src/oauth/oauth.service.ts
@@ -3,12 +3,19 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Client } from '../client/client.entity';
 import { randomBytes } from 'crypto';
+import { InMemoryAuthorizationCodeStrategy } from './code-autherization-strategy/in-memory-authorization-code.strategy';
+import { AuthorizationCodeStrategy } from './code-autherization-strategy/authorization-code-strategy.interface';
 
 @Injectable()
 export class OAuthService {
+  private codeStrategy: AuthorizationCodeStrategy;
+
   constructor(
     @InjectRepository(Client) private clientRepository: Repository<Client>,
-  ) {}
+  ) {
+    // Initially use the in-memory strategy
+    this.codeStrategy = new InMemoryAuthorizationCodeStrategy();
+  }
 
   async validateClient(
     clientId: string,
@@ -27,6 +34,16 @@ export class OAuthService {
     const code = randomBytes(16).toString('hex'); // Generates a 32-character hexadecimal string
 
     // TODO: Save the authorization code to the in-memory store
+    await this.codeStrategy.saveCode(
+      code,
+      {
+        client_id,
+        user_email,
+        redirect_uri,
+        scope,
+      },
+      600000,
+    ); // Add the missing ttl argument
 
     return code;
   }
